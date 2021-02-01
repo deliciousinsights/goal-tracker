2f30b776be7b65b8f225d5a1f580ab83bf0ebb85
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:48:34 +0100

    Étape 8 : Action Creators, Reducer et tests "Goals"

--- src/reducers/goals.js
+++ src/reducers/goals.js
@@ -4,7 +4,10 @@ import ObjectID from 'bson-objectid'
 // Action Creators
 // ---------------
 
-export const addGoal = createAction('goal-tracker/goals/addGoal')
+export const addGoal = createAction(
+  'goal-tracker/goals/addGoal',
+  (payload) => ({ payload: { ...payload, id: ObjectID().toHexString() } })
+)
 export const removeGoal = createAction('goal-tracker/goals/removeGoal')
 export const updateGoal = createAction('goal-tracker/goals/updateGoal')
 
@@ -14,13 +17,14 @@ export const updateGoal = createAction('goal-tracker/goals/updateGoal')
 export default createReducer([], (builder) => {
   builder
     .addCase(addGoal, (state, { payload }) => {
-      const id = ObjectID().toHexString()
-      state.push({ ...payload, id })
+      state.push(payload)
     })
     .addCase(removeGoal, (state, { payload }) => {
-      // FIXME
+      return state.filter(({ id }) => id !== payload.id)
     })
     .addCase(updateGoal, (state, action) => {
-      // FIXME
+      return state.map((goal) =>
+        goal.id === action.payload.id ? action.payload : goal
+      )
     })
 })
