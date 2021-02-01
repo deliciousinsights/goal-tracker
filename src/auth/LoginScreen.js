c9a77899787f4ad86db591d2917cacb8f7066e01
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:24:17 +0100

    Étape 1 : Bases HomeScreen et TrackerScreen

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -6,6 +6,48 @@ import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
 import TextField from '@mui/material/TextField'
 
+import classes from './LoginScreen.module.css'
+import TogglablePasswordField from './TogglablePasswordField'
+
 export default function LoginScreen() {
-  return <p>COMING SOON: LOGIN SCREEN</p>
+  return (
+    <form>
+      <Card className={classes.loginScreen}>
+        <CardHeader title='Goal Tracker' subheader='Connexion' />
+        <CardContent>
+          <TextField
+            autoComplete='home email'
+            fullWidth
+            id='email'
+            label='E-mail'
+            margin='normal'
+            name='email'
+            placeholder='mon@email.tld'
+            required
+            type='email'
+          />
+          <TogglablePasswordField
+            autoComplete='current-password'
+            fullWidth
+            id='password'
+            label='Mot de passe'
+            margin='normal'
+            name='password'
+            placeholder='super mot de passe'
+            required
+          />
+        </CardContent>
+        <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            color='primary'
+            startIcon={<ArrowForward />}
+            type='submit'
+            variant='contained'
+          >
+            Connecte-toi
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  )
 }
