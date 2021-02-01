ffa4421b794b805032e589eabd0f18fa42896f4d
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:45:55 +0100

    Étape 7 : Action Creators, Reducer et tests "Current User"

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,14 +1,19 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 // Action Creators
 // ---------------
 
-export const logIn = 'FIXME'
-export const logOut = 'FIXME'
+export const logIn = createAction('goal-tracker/currentUser/login')
+export const logOut = createAction('goal-tracker/currentUser/logOut')
 
 // Réducteur
 // ---------
 
-export default createReducer(null, (builder) => {
-  // FIXME
+export default createReducer({ loginState: 'logged-out' }, (builder) => {
+  builder
+    .addCase(logIn, (state, { payload: { email } }) => ({
+      loginState: 'logged-in',
+      email,
+    }))
+    .addCase(logOut, () => ({ loginState: 'logged-out' }))
 })
--- src/reducers/currentUser.spec.js
+++ src/reducers/currentUser.spec.js
@@ -1,9 +1,27 @@
 import reducer, { logIn, logOut } from './currentUser'
 
 describe('Current User reducer', () => {
-  it.todo('should return its initial state')
+  it('should return its initial state', () => {
+    const initialState = undefined
+    const expectedState = { loginState: 'logged-out' }
 
-  it.todo('should handle login')
+    expect(reducer(initialState, {})).toEqual(expectedState)
+  })
 
-  it.todo('should handle logout')
+  it('should handle login', () => {
+    const email = 'john@example.com'
+    const initialState = { loginState: 'logged-out' }
+    const expectedState = { loginState: 'logged-in', email }
+
+    expect(
+      reducer(initialState, logIn({ email, password: 'no fate' }))
+    ).toEqual(expectedState)
+  })
+
+  it('should handle logout', () => {
+    const initialState = { loginState: 'logged-in', email: 'john@example.com' }
+    const expectedState = { loginState: 'logged-out' }
+
+    expect(reducer(initialState, logOut())).toEqual(expectedState)
+  })
 })
