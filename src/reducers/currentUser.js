2f82408f3212ddcbd4abc33d6ed8e951abfb4240
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:45:07 +0100

    Début étape 7 : bases pour Action Creators, Reducer et tests "Current User"

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,14 @@
+import { createReducer } from '@reduxjs/toolkit'
+
+// Action Creators
+// ---------------
+
+export const logIn = 'FIXME'
+export const logOut = 'FIXME'
+
+// Réducteur
+// ---------
+
+export default createReducer(null, (builder) => {
+  // FIXME
+})
