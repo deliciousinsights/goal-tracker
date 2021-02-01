a66e6c0076de44442aee3da264c6b83b70357824
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:39:25 +0100

    Début étape 4 : GoalTrackerWidget et map() dans TrackerScreen

---
+++ src/main/GoalTrackerWidget.js
@@ -0,0 +1,42 @@
+import Add from '@mui/icons-material/Add'
+import Fab from '@mui/material/Fab'
+import ThumbUp from '@mui/icons-material/ThumbUp'
+import Typography from '@mui/material/Typography'
+
+import classes from './TrackerScreen.module.css'
+import { func, GoalPropType, nonNegativeInteger } from '../shared/prop-types'
+import Gauge from '../shared/Gauge'
+
+export default function GoalTrackerWidget({ FIXME }) {
+  const adderComponent =
+    target > progress ? (
+      <Fab color='secondary' size='small' aria-label={`Progresser sur ${name}`}>
+        <Add />
+      </Fab>
+    ) : (
+      <Fab disabled size='small' aria-label='Objectif atteint, bravo !'>
+        <ThumbUp />
+      </Fab>
+    )
+
+  return (
+    <div className={classes.goal}>
+      <div className={classes.summary}>
+        <Typography variant='h6' component='h2'>
+          {name}
+        </Typography>
+        <Gauge value={progress} max={target} />
+        <Typography component='small'>
+          {`${progress} ${units} sur ${target}`}
+        </Typography>
+      </div>
+      <div className={classes.cta}>{adderComponent}</div>
+    </div>
+  )
+}
+
+GoalTrackerWidget.propTypes = {
+  goal: GoalPropType.isRequired,
+  progress: nonNegativeInteger.isRequired,
+  onProgress: func,
+}
