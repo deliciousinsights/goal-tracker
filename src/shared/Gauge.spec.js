01ece22ec41c84e4eb5fe18a5c89770df1e51e61
Christophe Porteneuve <christophe@delicious-insights.com>
2021-04-01 14:26:26 +0200

    Début exercice 1 étape 5 : déclinaison tests Gauge

--- src/shared/Gauge.spec.js
+++ src/shared/Gauge.spec.js
@@ -3,7 +3,13 @@ import { render, screen } from '@testing-library/react'
 import Gauge from './Gauge'
 
 describe('<Gauge />', () => {
-  it.todo('should render appropriately')
+  it('should render appropriately', () => {
+    render(<Gauge value={50} />)
+    const progressBar = screen.getByRole('progressbar')
+    expect(progressBar).toHaveClass('MuiLinearProgress-determinate')
+    expect(progressBar).toHaveStyle({ height: '8px' })
+    expect(progressBar).toHaveAttribute('aria-valuenow', '50')
+  })
 
   it.todo('should normalize value on custom max')
 
