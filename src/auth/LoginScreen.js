95ea882d1eebd70e2841ca4654651b3caa8758fb
Christophe Porteneuve <christophe@delicious-insights.com>
2022-11-07 15:08:55 +0100

    Étape 6 : UX de l’écran de login (et état local)

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import ArrowForward from '@mui/icons-material/ArrowForward'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
@@ -9,9 +11,15 @@ import TextField from '@mui/material/TextField'
 import classes from './LoginScreen.module.css'
 import TogglablePasswordField from './TogglablePasswordField'
 
+const MIN_PASSWORD_LENGTH = 6
+
 export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const canLogIn = email !== '' && password.trim().length >= MIN_PASSWORD_LENGTH
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Card className={classes.loginScreen}>
         <CardHeader title='Goal Tracker' subheader='Connexion' />
         <CardContent>
@@ -22,24 +30,30 @@ export default function LoginScreen() {
             label='E-mail'
             margin='normal'
             name='email'
+            onChange={(event) => setEmail(normalizeEmail(event.target.value))}
             placeholder='mon@email.tld'
             required
             type='email'
+            value={email}
           />
           <TogglablePasswordField
             autoComplete='current-password'
             fullWidth
+            helperText={`${MIN_PASSWORD_LENGTH} caractères minimum`}
             id='password'
             label='Mot de passe'
             margin='normal'
             name='password'
+            onChange={(event) => setPassword(event.target.value)}
             placeholder='super mot de passe'
             required
+            value={password}
           />
         </CardContent>
         <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
           <Button
             color='primary'
+            disabled={!canLogIn}
             startIcon={<ArrowForward />}
             type='submit'
             variant='contained'
@@ -50,4 +64,15 @@ export default function LoginScreen() {
       </Card>
     </form>
   )
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log({ email, password })
+  }
+}
+
+function normalizeEmail(email) {
+  return email
+    .replace(/\s+/g, '')
+    .replace(/^[^@]+/, (text) => text.toLowerCase())
 }
